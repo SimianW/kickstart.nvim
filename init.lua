abd25276425acc68721f4aaba8706ab5637a1267
Simian Wang <wangsimian2020@outlook.com>
2026-05-24 18:32:06 -0400

    fix: migrate diagnostic jump to on_jump API (fix deprecation warning)
    - Replace deprecated  with
    - Use scope = 'cursor' and focus = false for same float behavior
    - Also fix typo: 'Teest' -> 'Text' in virtual_lines comment

--- init.lua
+++ init.lua
@@ -200,10 +200,18 @@ do
 
     -- Can switch between these as you prefer
     virtual_text = true, -- Text shows up at the end of the line
-    virtual_lines = false, -- Teest shows up underneath the line, with virtual lines
+    virtual_lines = false, -- Text shows up underneath the line, with virtual lines
 
     -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
-    jump = { float = true },
+    jump = {
+      on_jump = function(_, bufnr)
+        vim.diagnostic.open_float {
+          bufnr = bufnr,
+          scope = 'cursor',
+          focus = false,
+        }
+      end,
+    },
   }
 
   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
